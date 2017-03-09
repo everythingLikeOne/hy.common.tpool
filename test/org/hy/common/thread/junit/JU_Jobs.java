60848463b426168e9dab68aa31e614c4dd486073
Administrator <Administrator@PC-201607120928>
2017-03-09 16:39:34 +0800

    ：改成==

--- test/org/hy/common/thread/junit/JU_Jobs.java
+++ test/org/hy/common/thread/junit/JU_Jobs.java
@@ -59,7 +59,7 @@ public class JU_Jobs
     public void test_Job_Execute()
     {
         ThreadPool.sleep(5 * 1000);
-        System.out.println("Job Time: " + new Date());
+        System.out.println("Job Time== " + new Date());
     }
     
 }
